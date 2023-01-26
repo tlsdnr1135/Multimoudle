683c85bfb14e8c77488ad756352672afbad78e93
DESKTOP-G9LPVC0\dev <tlsdnr1135@naver.com>
2023-01-26 10:43:26 +0900

    피드백 수정 완료.

--- api_common/src/main/java/com/center/module/CommonApplicationTests.java
+++ api_common/src/main/java/com/center/module/CommonApplicationTests.java
@@ -1,8 +1,8 @@
-package com.center.module;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonApplicationTests {
-    public void contextLoads(){}
-}
+//package com.center.module;
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CommonApplicationTests {
+//    public void contextLoads(){}
+//}
